481790990e5d2002ad7ecb6457a7bfd954cf474b
babunova <anton.babunov@strawbinary.de>
2022-06-14 22:31:52 +0200

    yes

--- script.js
+++ script.js
@@ -4,16 +4,20 @@ const getRandomNFromArray = (n, array) => {
   return array.sort(() => 0.5 - Math.random()).slice(0, n);
 };
 
-// Replace ./data.json with your JSON feed
 fetch("./data/backdrops.json")
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    const stuff = getRandomNFromArray(15, data);
-    console.log(stuff);
-    NetflixSlider(".popular", stuff);
+    const popular = getRandomNFromArray(15, data);
+    NetflixSlider(".popular", popular);
+
+    const mylist = getRandomNFromArray(15, data);
+    NetflixSlider(".mylist", mylist);
+
+    const watchagain = getRandomNFromArray(15, data);
+    NetflixSlider(".watchagain", watchagain);
+
   })
   .catch((err) => {
-    // Do something for an error here
   });
