b221f490f96ff6aa050d0ec22ae9b261492fdfa3
User1 <git@walzenbach.com>
2022-06-14 10:10:34 +0200

    stand praktikum

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+import NetflixSlider from "./movieListSlider.js";
+
 const getRandomNFromArray = (n, array) => {
-  return array.sort(() => 0.5 - Math.random()).slice(0, n)
-}
+  return array.sort(() => 0.5 - Math.random()).slice(0, n);
+};
+
+// Replace ./data.json with your JSON feed
+fetch("./data/backdrops.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const stuff = getRandomNFromArray(15, data);
+    console.log(stuff);
+    NetflixSlider(".popular", stuff);
+  })
+  .catch((err) => {
+    // Do something for an error here
+  });
